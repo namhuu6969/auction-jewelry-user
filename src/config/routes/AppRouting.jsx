f09784233a87850d50bede2ebd2422b541b2407c
namhuu6969 <namquangbinh6969@gmail.com>
2024-05-24 22:34:01 +0700

    Fix Structure for product list

--- src/config/routes/AppRouting.jsx
+++ src/config/routes/AppRouting.jsx
@@ -1,14 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from '@pages/AuctionPage/components/Home';
-import { JewelryList } from '@pages/AuctionPage/components/ProductList';
 import { AuctionPage } from '@pages/AuctionPage';
+import { ProductList } from '@pages/AuctionPage/components/ProductList';
+import { JewelryList } from '@pages/AuctionPage/components/ProductList/components/JewelryList/JewelryList';
 
 const AppRouting = () => {
   return (
     <Routes>
       <Route path='/' element={<AuctionPage />}>
         <Route path='' element={<Home />}></Route>
-        <Route path='jewelry' element={<JewelryList />}></Route>
+        <Route path='jewelry' element={<ProductList />}>
+          <Route path='' element={<JewelryList />} />
+        </Route>
       </Route>
     </Routes>
   );
