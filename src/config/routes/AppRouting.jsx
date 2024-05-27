972f886a5134dd128602f962d4dfe40db11a14d0
namhuu6969 <namquangbinh6969@gmail.com>
2024-05-27 14:44:45 +0700

    BaseUI for login page

--- src/config/routes/AppRouting.jsx
+++ src/config/routes/AppRouting.jsx
@@ -3,10 +3,12 @@ import { Home } from '@pages/AuctionPage/components/Home';
 import { AuctionPage } from '@pages/AuctionPage';
 import { ProductList } from '@pages/AuctionPage/components/ProductList';
 import { JewelryList } from '@pages/AuctionPage/components/ProductList/components/JewelryList/JewelryList';
+import { Login } from '../../pages/Login';
 
 const AppRouting = () => {
   return (
     <Routes>
+      <Route path='/login' element={<Login />} />
       <Route path='/' element={<AuctionPage />}>
         <Route path='' element={<Home />}></Route>
         <Route path='jewelry' element={<ProductList />}>
