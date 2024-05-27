b5dd8dbfb38806ae5c61dd6314144dfe1b99f50d
tanhnm <hnmtan03@gmail.com>
2024-05-27 13:51:39 +0700

    Fix Style CardNews

--- src/components/ui/CardNews.jsx
+++ src/components/ui/CardNews.jsx
@@ -1,15 +1,22 @@
-import { Card } from 'antd';
-
-const { Meta } = Card;
+import { Card, Flex, Typography } from 'antd';
+const { Title } = Typography;
 
 const CardNews = ({ imageUrl, title, description }) => {
   return (
     <Card
-      hoverable
-      cover={<img alt={title} src={imageUrl} />}
-      style={{ width: 300, margin: 'auto' }}
+      cover={<img alt={title} src={imageUrl} className='w-full object-cover' />}
+      bordered={false}
+      bodyStyle={{ padding: 0 }}
+      className='!w-full !h-full'
     >
-      <Meta title={title} description={description} />
+      <Flex className='!min-h-full bg-[#F2E7E7] px-5 py-4' vertical>
+        <Title className='multi-line-ellipsis !flex-1 text-left font-serif' level={4}>
+          {title}
+        </Title>
+        <Title level={5} className='!flex-1 !text-[#898989] !font-medium text-left font-sans'>
+          {description}
+        </Title>
+      </Flex>
     </Card>
   );
 };
