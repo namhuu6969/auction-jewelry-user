1a96b23d65a050d3e5a27bd002b161d8e88168fc
tanhnm <hnmtan03@gmail.com>
2024-05-30 22:09:21 +0700

    fix Header and Layout adding hover

--- src/config/routes/AppRouting.jsx
+++ src/config/routes/AppRouting.jsx
@@ -5,14 +5,16 @@ import { ProductList } from '@pages/AuctionPage/components/ProductList';
 import { JewelryList } from '@pages/AuctionPage/components/ProductList/components/JewelryList/JewelryList';
 import { Login } from '@pages/LoginPage';
 import { Register } from '@pages/RegisterPage';
+import { About } from '@pages/AuctionPage/components/About';
 
 const AppRouting = () => {
   return (
     <Routes>
       <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />}/>
+      <Route path='/register' element={<Register />} />
       <Route path='/' element={<AuctionPage />}>
         <Route path='' element={<Home />}></Route>
+        <Route path='about' element={<About />}></Route>
         <Route path='jewelry' element={<ProductList />}>
           <Route path='' element={<JewelryList />} />
         </Route>
