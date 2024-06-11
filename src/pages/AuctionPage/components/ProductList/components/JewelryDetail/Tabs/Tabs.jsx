cba76350e600424d1ef8ab3a5f4bf508903f930a
tanhnm <hnmtan03@gmail.com>
2024-06-12 01:11:44 +0700

    Fixing content with API response and adding logic functional for bidding

--- src/pages/AuctionPage/components/ProductList/components/JewelryDetail/Tabs/Tabs.jsx
+++ src/pages/AuctionPage/components/ProductList/components/JewelryDetail/Tabs/Tabs.jsx
@@ -1,161 +1,97 @@
-import { Flex, Tabs, Typography } from 'antd';
+import { Flex, Tabs, Typography, Image } from 'antd';
 import './index.css';
-const { Title } = Typography;
 
-const TabsContent = () => {
+const { Title, Paragraph } = Typography;
+
+const TabsContent = ({ jewelry, startTime, endTime }) => {
   const items = [
     {
       key: 1,
-      label: 'Auction Description',
+      label: <p className='!font-medium text-black text-lg'>Description</p>,
       children: (
-        <>
-          <Flex className=''>
-            <Title
-              className='w-[10%] text-left font-sans !font-medium'
-              level={5}
-            >
-              Seller:
-            </Title>
-            <Title
-              level={5}
-              className='!m-0 text-left font-sans !font-medium !text-red-600'
-            >
-              Huu Nam
-            </Title>
-          </Flex>
-          <Flex className=''>
-            <Title
-              className='w-[10%] text-left font-sans !font-medium'
-              level={5}
-            >
-              Start Time:
-            </Title>
-            <Title
-              level={5}
-              className='!m-0 text-left font-sans !font-medium !text-red-600'
-            >
-              20/05/2024
-            </Title>
-          </Flex>
-          <Flex className=''>
-            <Title
-              className='w-[10%] text-left font-sans !font-medium'
-              level={5}
-            >
-              End time:
-            </Title>
-            <Title
-              level={5}
-              className='!m-0 text-left font-sans !font-medium !text-red-600'
-            >
-              23/05/2024
-            </Title>
-          </Flex>
-          <Flex className=''>
-            <Title
-              className='w-[10%] text-left font-sans !font-medium'
-              level={5}
-            >
-              Buy now pricing:
-            </Title>
-            <Title
-              level={5}
-              className='!m-0 text-left font-sans !font-medium !text-red-600'
-            >
-              5.000.000 VND
-            </Title>
-          </Flex>
-        </>
+        <div>
+          <Paragraph className='text-lg'>{jewelry.description}</Paragraph>
+        </div>
       ),
     },
     {
       key: 2,
-      label: 'Jewelry Description',
+      label: <p className='!font-medium text-black text-lg'>Information</p>,
       children: (
-        <>
-          <Flex className=''>
-            <Title
-              className='w-[10%] text-left font-sans !font-medium'
-              level={5}
-            >
-              Name:
-            </Title>
-            <Title level={5} className='!m-0 text-left font-sans !font-medium'>
-              Meoo meoo
-            </Title>
+        <div className='information'>
+          <Flex className='justify-start items-center'>
+            <p className='text-lg mr-2'>Starting Time:</p>
+            <p className='text-lg'>{new Date(startTime).toLocaleString()}</p>
           </Flex>
-          <Flex className=''>
-            <Title
-              className='w-[10%] text-left font-sans !font-medium'
-              level={5}
-            >
-              Description:
-            </Title>
-            <Title level={5} className='!m-0 text-left font-sans !font-medium'>
-              Meoo meoo
-            </Title>
+          <Flex className='justify-start items-center'>
+            <p className='text-lg mr-2'>Ending Time:</p>
+            <p className='text-lg'>{new Date(endTime).toLocaleString()}</p>
           </Flex>
-          <Flex className=''>
-            <Title
-              className='w-[10%] text-left font-sans !font-medium'
-              level={5}
-            >
-              Category:
-            </Title>
-            <Title level={5} className='!m-0 text-left font-sans !font-medium'>
-              Meoo meoo
-            </Title>
+          <Flex className='justify-start items-center'>
+            <p className='text-lg mr-2'>Category:</p>
+            <p className='text-lg'>{jewelry.category.name}</p>
           </Flex>
-          <Flex className=''>
-            <Title
-              className='w-[10%] text-left font-sans !font-medium'
-              level={5}
-            >
-              Material:
-            </Title>
-            <Title level={5} className='!m-0 text-left font-sans !font-medium'>
-              Meoo meoo
-            </Title>
+          <Flex className='justify-start items-center'>
+            <p className='text-lg mr-2'>Weight:</p>
+            <p className='text-lg'>{jewelry.weight} grams</p>
           </Flex>
-          <Flex className=''>
-            <Title
-              className='w-[10%] text-left font-sans !font-medium'
-              level={5}
-            >
-              Color:
-            </Title>
-            <Title level={5} className='!m-0 text-left font-sans !font-medium'>
-              Meoo meoo
-            </Title>
+          <Flex className='justify-start items-center'>
+            <p className='text-lg mr-2'>Size:</p>
+            <p className='text-lg'>{jewelry.size}</p>
           </Flex>
-          <Flex className=''>
-            <Title
-              className='w-[10%] text-left font-sans !font-medium'
-              level={5}
-            >
-              Brand:
-            </Title>
-            <Title level={5} className='!m-0 text-left font-sans !font-medium'>
-              Meoo meoo
-            </Title>
+          <Flex className='justify-start items-center'>
+            <p className='text-lg mr-2'>Color:</p>
+            <p className='text-lg'>{jewelry.color}</p>
           </Flex>
-          <Flex className=''>
-            <Title
-              className='w-[10%] text-left font-sans !font-medium'
-              level={5}
-            >
-              Condition:
-            </Title>
-            <Title level={5} className='!m-0 text-left font-sans !font-medium'>
-              Meoo meoo
-            </Title>
+          <Flex className='justify-start items-center'>
+            <p className='text-lg mr-2'>Sex:</p>
+            <p className='text-lg'>{jewelry.sex}</p>
           </Flex>
-        </>
+          <Flex className='justify-start items-center'>
+            <p className='text-lg mr-2'>Brand:</p>
+            <p className='text-lg'>{jewelry.brand.name}</p>
+          </Flex>
+          <Flex className='justify-start items-center'>
+            <p className='text-lg mr-2'>Condition:</p>
+            <p className='text-lg'>{jewelry.jewelryCondition}</p>
+          </Flex>
+          <Flex className='justify-start items-center'>
+            <p className='text-lg mr-2'>Status:</p>
+            <p className='text-lg'>{jewelry.status}</p>
+          </Flex>
+          <Flex className='justify-start items-center'>
+            <p className='text-lg mr-2'>Collection:</p>
+            <p className='text-lg'>{jewelry.collection.name}</p>
+          </Flex>
+          <Flex className='justify-start items-center'>
+            <p className='text-lg mr-2'>Materials:</p>
+            <Flex className='!w-[300px]' justify='space-between'>
+              {jewelry.jewelryMaterials.map((material, index) => (
+                <p key={index} className='text-lg'>
+                  {`${material.material.name} - ${material.weight} grams`}
+                </p>
+              ))}
+            </Flex>
+          </Flex>
+        </div>
       ),
     },
+    {
+      key: 3,
+      label: <p className='!font-medium text-black text-lg'>History</p>,
+      children: <p className='text-lg'>History content here</p>,
+    },
   ];
+
   return (
-    <Tabs defaultActiveKey='1' className='custom-tabs-detail' items={items} />
+    <div className='w-full mt-5'>
+      <Flex className='w-full justify-between'>
+        <Title level={2} className='!font-serif !font-medium'>
+          Jewelry Description
+        </Title>
+      </Flex>
+      <Tabs defaultActiveKey='1' items={items} />
+    </div>
   );
 };
 
--- src/pages/AuctionPage/components/ProductList/components/JewelryDetail/index.jsx
+++ src/pages/AuctionPage/components/ProductList/components/JewelryDetail/index.jsx
@@ -5,24 +5,39 @@ import Carousel from '@components/ui/carousel/Carousel';
 import { StarFilled, UserOutlined, ClockCircleOutlined } from '@ant-design/icons';
 import TabsContent from './Tabs/Tabs';
 import './index.css';
-import { useState } from 'react';
-import { SecondaryButton } from '@components/ui/SecondaryButton';
+import { useState, useEffect } from 'react';
 import { PrimaryButton } from '@components/ui/PrimaryButton';
 import { BidModal } from './BidModal/BidModal'; // Adjust the import path as needed
 import { useNavigate } from 'react-router-dom';
+import { auctionApi } from '../../../../../../services/api/AuctionApi/AuctionApi';
 
 const { Title } = Typography;
 
 export const JewelryDetail = () => {
   const navigator = useNavigate();
   const { id } = useParams();
-  const [selectedImage, setSelectedImage] = useState({
-    id: 1,
-    image: 'https://happyjewelers.com/cdn/shop/products/130-40289_1_1445x.jpg?v=1687387508',
-  });
+  const [auctionData, setAuctionData] = useState(null);
+  const [jewelryData, setJewelryData] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [currentStep, setCurrentStep] = useState(0);
   const [bidAmount, setBidAmount] = useState('');
+  const [currentStep, setCurrentStep] = useState(0);
+  const [step, setStep] = useState(0);
+
+  useEffect(() => {
+    const fetchAuctionData = async () => {
+      const response = await auctionApi.getAuctionById(id);
+      setJewelryData(response.data.jewelry);
+      setAuctionData(response.data);
+      setStep(response.data.step);
+      setSelectedImage({
+        id: 1,
+        image: `http://localhost:8080/uploads/jewelry/${response.data.jewelry.jewelryImages[0].url}`, // Set the initial selected image
+      });
+      // console.log(response.data);
+    };
+    fetchAuctionData();
+  }, [id]);
 
   const showModal = () => {
     setIsModalVisible(true);
@@ -51,7 +66,7 @@ export const JewelryDetail = () => {
 
   const increaseBidAmount = () => {
     setBidAmount((prevBidAmount) => {
-      return parseInt(prevBidAmount) + 100;
+      return parseInt(prevBidAmount) + step;
     });
   };
 
@@ -68,6 +83,21 @@ export const JewelryDetail = () => {
 
   const prev = () => {
     setCurrentStep(currentStep - 1);
+  };
+
+  const formatedDateTime = (time) => {
+    const dateFormat = new Date(time).toLocaleString('vi-VN', {
+      timeZone: 'Asia/Ho_Chi_Minh',
+    });
+    return dateFormat;
+  };
+
+  const calculateDaysDifference = (startDate, endDate) => {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    const differenceInTime = end.getTime() - start.getTime();
+    const differenceInDays = Math.ceil(differenceInTime / (1000 * 3600 * 24));
+    return differenceInDays;
   };
 
   const breadcumLink = [
@@ -97,29 +127,17 @@ export const JewelryDetail = () => {
     );
   };
 
-  const data = [
-    {
-      id: 1,
-      image: 'https://happyjewelers.com/cdn/shop/products/130-40289_1_1445x.jpg?v=1687387508',
-    },
-    {
-      id: 2,
-      image: 'https://c0.wallpaperflare.com/preview/984/867/753/jewellery-gold-wedding-indian.jpg',
-    },
-    {
-      id: 3,
-      image:
-        'https://png.pngtree.com/thumb_back/fh260/back_our/20190621/ourmid/pngtree-taobao-jewelry-fresh-and-simple-gold-jewelry-poster-image_194638.jpg',
-    },
-    {
-      id: 4,
-      image: 'https://c0.wallpaperflare.com/preview/984/867/753/jewellery-gold-wedding-indian.jpg',
-    },
-  ];
-
   const handleImageClick = (imageUrl) => {
     setSelectedImage(imageUrl);
   };
+
+  if (!jewelryData) {
+    return <div>Loading...</div>;
+  }
+
+  const { name, jewelryImages, staringPrice, sellerId } = jewelryData;
+
+  const { currentPrice, endTime, startTime, status } = auctionData;
 
   return (
     <div className='container mx-auto'>
@@ -134,25 +152,40 @@ export const JewelryDetail = () => {
                 src={selectedImage.image}
               />
             </div>
-            <Carousel component={img} data={data} numberOfSilde={5} />
+            {jewelryImages.length > 1 && (
+              <Carousel
+                component={img}
+                data={jewelryImages.map((img) => ({
+                  id: img.id,
+                  image: `http://localhost:8080/uploads/jewelry/${img.url}`,
+                }))}
+                numberOfSilde={5}
+              />
+            )}
           </Flex>
           <Flex vertical className='w-[50%]' gap={20}>
             <Title level={2} className='font-serif !font-medium text-left'>
-              Jewel ring
+              {name}
             </Title>
             <Flex className='items-center w-full'>
               <div className='grid grid-cols-2 gap-y-5'>
-                <Title className='!m-0 !my-auto text-left font-sans !font-medium' level={4}>
-                  The current:
+                <Title className='!mr-4 !my-auto text-left font-sans !font-medium' level={4}>
+                  Giá thầu hiện tại:
                 </Title>
                 <Title className='!m-0 !my-auto !text-red-600 text-left font-sans' level={3}>
-                  1.000.000 VND
+                  {currentPrice} VND
                 </Title>
                 <Title className='!m-0 !my-auto text-left font-sans !font-medium' level={4}>
-                  Step:
+                  Bước nhảy:
                 </Title>
                 <Title className='!m-0 !my-auto !text-red-600 text-left font-sans' level={3}>
-                  500.000 VND
+                  {step} VND
+                </Title>
+                <Title className='!m-0 !my-auto text-left font-sans !font-medium' level={4}>
+                  Trạng thái:
+                </Title>
+                <Title className='!m-0 !my-auto !text-red-600 text-left font-sans' level={3}>
+                  {status}
                 </Title>
               </div>
               <div className='w-[40%] p-5'>
@@ -174,7 +207,7 @@ export const JewelryDetail = () => {
               <Flex className='items-center' gap={10}>
                 <ClockCircleOutlined className='!text-3xl' />
                 <Title level={4} className='!m-0 font-sans !font-thin'>
-                  3 days
+                  {calculateDaysDifference(startTime, endTime)} days
                 </Title>
               </Flex>
               <Flex className='items-center' gap={10}>
@@ -182,19 +215,23 @@ export const JewelryDetail = () => {
                   level={5}
                   className='bg-[#DED6D6] !m-0 px-2 font-sans !font-medium rounded-md'
                 >
-                  Session end at 20/05/2024
+                  Session end at {formatedDateTime(endTime)}
                 </Title>
               </Flex>
             </Flex>
             <PrimaryButton onClick={showModal} className={'!text-2xl font-medium'}>
               Đấu giá
             </PrimaryButton>
-            <div className='flex items-center gap-5'>
-              <SecondaryButton onClick={() => console.log('oke')}>Mua ngay</SecondaryButton>
-              <Title level={4} className='!m-0 !text-red-600'>
-                with 5.000.000VND
-              </Title>
-            </div>
+            {staringPrice > 0 && (
+              <div className='w-full p-5'>
+                <Button
+                  icon={<StarFilled className='text-yellow-400' />}
+                  className='font-sans !shadow-lg hover:!border-[1px] hover:!border-solid hover:!border-black hover:!text-black !border-black'
+                >
+                  Buy Now
+                </Button>
+              </div>
+            )}
             <div className='flex mt-5 flex-col border border-gray-300 py-5 px-10 rounded-lg'>
               <Title level={3} className='text-left font-serif'>
                 Seller
@@ -203,18 +240,19 @@ export const JewelryDetail = () => {
                 <Avatar size={64} icon={<UserOutlined />} />
                 <Flex vertical className='justify-center'>
                   <Title level={4} className='font-sans !font-medium'>
-                    Nam
+                    {sellerId.full_name}
                   </Title>
                 </Flex>
               </Flex>
             </div>
           </Flex>
         </Flex>
-        <TabsContent />
+        <TabsContent jewelry={jewelryData} startTime={startTime} endTime={endTime} />
       </Flex>
       <BidModal
         isVisible={isModalVisible}
         currentStep={currentStep}
+        step={step}
         bidAmount={bidAmount}
         handleOk={handleOk}
         userWallet={1000}
