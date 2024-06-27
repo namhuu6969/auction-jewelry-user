44791248a54873e1f3666c2ff619f83982c12de2
namhuu6969 <namquangbinh6969@gmail.com>
2024-06-27 14:58:09 +0700

    Fix verified logic

--- src/pages/VerifiedPage/index.jsx
+++ src/pages/VerifiedPage/index.jsx
@@ -1,15 +1,46 @@
 import { Result } from 'antd';
 import { PrimaryButton } from '../../components/ui/PrimaryButton';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export const VerifiedPage = () => {
   const navigate = useNavigate();
-  return (
+  const [status, setStatus] = useState(false);
+  const { token, userId } = useParams();
+  const [message, setMessage] = useState('');
+  useEffect(() => {
+    const fetchVerify = async () => {
+      try {
+        await axios.get(
+          `http://apijewelryauction.techx.id.vn:8081/api/v1/user/verify?userId=${userId}&token=${token}`
+        );
+        setStatus(true);
+      } catch (error) {
+        setStatus(false);
+        setMessage(error.response.data.message);
+      }
+    };
+    fetchVerify();
+  }, [token, userId]);
+  return status ? (
     <Result
       className='!font-sans !h-[58vh] flex flex-col justify-center'
       status='success'
       title='Verify Success!'
-      subTitle='Now you can login with your account'
+      subTitle='Please check your account '
+      extra={[
+        <PrimaryButton key={'login'} onClick={() => navigate('/login')}>
+          Go to login
+        </PrimaryButton>,
+      ]}
+    />
+  ) : (
+    <Result
+      className='!font-sans !h-[58vh] flex flex-col justify-center'
+      status='error'
+      title='Verify Error!'
+      subTitle={message + '! Please register again'}
       extra={[
         <PrimaryButton key={'login'} onClick={() => navigate('/login')}>
           Go to login
